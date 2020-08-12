20b8331fd8419717d4879df822673cdcbdc43bec
Nataliya Bondarenko <nataliyabondarenko@MacBook-Pro-Nataliya.local>
2020-08-12 11:39:24 +0300

    hw 3 is updated

--- javascript/goit-js-hw-03/task-03.js
+++ javascript/goit-js-hw-03/task-03.js
@@ -6,33 +6,37 @@ const findBestEmployee = function (employees) {
   for (let i = 1; i < keys.length; i += 1) {
     if (employees[keys[i]] > employees[theMostProdEmpl]) {
       theMostProdEmpl = keys[i];
-      console.log(theMostProdEmpl);
     }
   }
-  console.log(theMostProdEmpl);
+  return theMostProdEmpl;
 };
 
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
 
-findBestEmployee({
-  ann: 29,
-  david: 35,
-  helen: 1,
-  lorence: 99,
-});
-// lorence
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  })
+); // lorence
 
-findBestEmployee({
-  poly: 12,
-  mango: 17,
-  ajax: 4,
-}); // mango
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  })
+); // mango
 
-findBestEmployee({
-  lux: 147,
-  david: 21,
-  kiwi: 19,
-  chelsy: 38,
-}); // lux
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  })
+); // lux
